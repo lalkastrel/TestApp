d9c1693cbe13b41ab9129e6295746d20c95a4f1c
You <lalkastrel@gmail.com>
2025-11-10 01:14:11 +0300

    first change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MenuBar } from './components/menu-bar/menu-bar'
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
